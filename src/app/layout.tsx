12f6314c4a7afc4eb9684fdf9d7a5eabc51fdd45
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-10-12 19:51:28 +0530

    theme fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -164,8 +164,7 @@ export default function RootLayout({
       >
        <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
-            enableSystem
+            forcedTheme="dark"
             disableTransitionOnChange
           >
           {children}
