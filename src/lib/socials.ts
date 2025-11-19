9523477b869c5d3d297d5ee736cda915e1cc7675
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-11-19 20:39:32 +0530

    Refactor layout component to improve structure and integrate environment variables for analytics

---
+++ src/lib/socials.ts
@@ -0,0 +1,15 @@
+interface Social {
+    x: string;
+    github: string;
+    linkedin: string;
+    email: string;
+    resume: string;
+}
+
+export const socials: Readonly<Social> = {
+    x: "https://x.com/punyakrit_22",
+    github: "https://github.com/punyakrit",
+    linkedin: "https://www.linkedin.com/in/punyakrit-singh-makhni/",
+    email: "punyakritsinghmakhni@gmail.com",
+    resume: "https://drive.google.com/file/d/1Bd8AEiYk9OyTV6Oe0UqGDjuLKLjZcqcK/view?usp=sharing",
+};
