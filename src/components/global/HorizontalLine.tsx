62b5b45170a1386195d63a2bcbddb8b857eee446
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-11-22 23:10:50 +0530

    line ui fix

--- src/components/global/HorizontalLine.tsx
+++ src/components/global/HorizontalLine.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HorizontalLine() {
   return (
-    <div className='h-[2px] w-full bg-[#1B1B1B] md:my-16 my-8'>
+    <div className='h-[2px] w-full dark:bg-[#1B1B1B] bg-[#f0f0f0] md:my-16 my-8'>
 
     </div>
   )
