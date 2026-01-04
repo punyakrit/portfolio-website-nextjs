8e05e90db647931c026661faa1c6cf04e0620450
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2026-01-04 23:31:53 +0530

    remove snowfall

--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -5,7 +5,7 @@ import Snowfall from 'react-snowfall'
 function Provider({ children }: { children: React.ReactNode }) {
   return (
     <div>
-        <Snowfall style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1000 , opacity: 0.5}} />
+        {/* <Snowfall style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1000 , opacity: 0.5}} /> */}
         {children}
     </div>
   )
